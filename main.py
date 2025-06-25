cdacb9c758d279acf3972c60ec3e5a3a7d1fdaca
vlad <138448988+gearupp@users.noreply.github.com>
2025-06-25 12:10:51 +0100

    Input Validation sorted, and image selected now processed by Ollama

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import os
 import ollama
+import pandas as pd
+
 
 images = [file for file in os.listdir("./images") if file.lower().endswith(('.png', '.jpg', '.jpeg'))]
 
+print(f" List of availible images to analyse: {images}")
+
+#printing list of images to analyse with index values
+print("Index  | Image name                   |")
 for image in images:
+    index = images.index(image)+1
+    print(f"   {index}   | {image}")
+
+
+
+def analysingfood(image):
     print(f"Analysing Image: {image}")
     response = ollama.chat(
-        model="llava:7b",
+    model="llava:7b",
         messages = [{
             "role": "user",
             "Content": "Find a meal and described what it contains only, not other descriptions required ",
         "images": [f"./images/{image}"]
         }])
-
-
     meal_description = response["message"]["content"]
     print(f"Meal Description: ", meal_description)
     res_calories = ollama.chat(
@@ -25,4 +35,12 @@ for image in images:
     calories_response = res_calories["message"]["content"]
     print(f"Calories for the meal: {image} is: {calories_response}")
 
-
+while True:
+    try:
+        selection = int(input("Select the image to analyse:"))
+        if selection & 1 < selection < (len(images) + 1):
+            analysingfood(images[selection - 1])
+            break
+    except ValueError:
+        print("Select a index # number only")
+        print(len(images))
